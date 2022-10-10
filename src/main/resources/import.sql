e6aa14bd1a9c9e59f43daecfed2dccbc2680beb3
Laura <lauragarciacastelao@hotmail.com>
2022-10-10 16:04:04 +0200

    Changes in import-sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
 ALTER TABLE letters AUTO_INCREMENT = 1;
 
-INSERT INTO letters (email,name) values ("gandalf@mail.com","Gandalf");
-INSERT INTO letters (email,name) values ("frodo@mail.com","Frodo");
-INSERT INTO letters (email,name) values ("aragonr@mail.com","Aragorn");
+INSERT INTO letters (email,name) values ("departamento@mail.com","Departamento");
+INSERT INTO letters (email,name) values ("institucion@mail.com","Inst");
+INSERT INTO letters (email,name) values ("educación@mail.com","Educa");
