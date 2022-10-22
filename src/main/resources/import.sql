e7b988657d8395fe9bd76fdcab8ac430d285d430
Laura <lauragarciacastelao@hotmail.com>
2022-10-22 16:15:41 +0200

    Quit not valid characters

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,13 +1,13 @@
 ALTER TABLE letters AUTO_INCREMENT = 1;
 
 INSERT INTO letters (email,name) values ("Cuanto tardais en responder?","Respondemos aproximadamente en el plazo de 24 horas");
-INSERT INTO letters (email,name) values ("Servicios que ofreceis","Contamos con diferentes propuestas, podrás enviar cartas,quejas o reclamaciones o preguntas");
-INSERT INTO letters (email,name) values (" El chat es un robot?","No, se utiliza una plataforma llamada Tidio, la persona que te atiende es real. En caso de que el servicio no este disponible se enviará unicamente un mensaje génerico.");
-INSERT INTO letters (email,name) values (" Puedo registrarme?","No.El proceso de registro actualmente no está disponible.");
-INSERT INTO letters (email,name) values (" Eliminais preguntas?","No, todas las preguntas se consideran válidas. Solo se elimina el spam.");
-INSERT INTO letters (email,name) values (" Que diferencia hay en hacer pregunta o preguntas frecuentes"," La pregunta que hagas tendrá respuesta.La sección de preguntas frecuentes se actualizará en preguntas frecuentes en función a la cantidad de preguntas repetidas");
-INSERT INTO letters (email,name) values ("Como llega mi carta?","La carta se derivará al departamento correspondiente");
-INSERT INTO letters (email,name) values ("Por donde me llega la respuesta?","Vía correo electrónico o en caso de que el usuario no tenga, llamada telefónica");
+INSERT INTO letters (email,name) values ("Servicios que ofreceis","Contamos con diferentes propuestas, podras enviar cartas,quejas o reclamaciones o preguntas");
+INSERT INTO letters (email,name) values (" El chat es un robot?","No, se utiliza una plataforma llamada Tidio, la persona que te atiende es real. En caso de que el servicio no este disponible se enviara unicamente un mensaje generico.");
+INSERT INTO letters (email,name) values (" Puedo registrarme?","No.El proceso de registro actualmente no esta disponible.");
+INSERT INTO letters (email,name) values (" Eliminais preguntas?","No, todas las preguntas se consideran validas. Solo se elimina el spam.");
+INSERT INTO letters (email,name) values (" Que diferencia hay en hacer pregunta o preguntas frecuentes"," La pregunta que hagas tendra respuesta.La seccion de preguntas frecuentes se actualizara en preguntas frecuentes en funcion a la cantidad de preguntas repetidas");
+INSERT INTO letters (email,name) values ("Como llega mi carta?","La carta se deriva al departamento correspondiente");
+INSERT INTO letters (email,name) values ("Por donde me llega la respuesta?","Via correo electronico o en caso de que el usuario no tenga, llamada telefonica");
 INSERT INTO question (pregunta,respuesta) values ("¿El chat es real?", "hola");
 
 
